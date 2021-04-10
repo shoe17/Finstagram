c3334cfa2318271c8650c6842ca22b16389da1f3
Darian Low <dlow3@jhu.edu>
2021-04-09 19:32:32 -0500

    fixed row height issue and ithumbnail dimensions issue

--- Finstagram/Controllers/NewPostViewController.swift
+++ Finstagram/Controllers/NewPostViewController.swift
@@ -11,15 +11,22 @@ import PhotosUI
 class NewPostViewController: UIViewController{
     
     @IBOutlet var newPostTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         newPostTableView.delegate = self
         newPostTableView.dataSource = self
         newPostTableView.rowHeight = 50
         newPostTableView.register(UINib(nibName: "ThumbnailAndCaptionCell", bundle: nil), forCellReuseIdentifier: "ThumbnailAndCaptionCell")
+        newPostTableView.allowsSelection = true
         
     }
     
+    @IBAction func shareButtonPressed(_ sender: UIBarButtonItem) {
+        let cell = newPostTableView.cellForRow(at: IndexPath(row: 0, section: 0)) as! ThumbnailAndCaptionCell
+        print(cell.thumbnail!)
+        print(cell.caption!)
+    }
 }
 
 //MARK: - UITableViewDataSource and UITableViewDelegate
@@ -27,18 +34,27 @@ class NewPostViewController: UIViewController{
 extension NewPostViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 100
+        }
+        return 50
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ThumbnailAndCaptionCell", for: indexPath) as! ThumbnailAndCaptionCell
-        cell.thumbnailAndCaptionCellDelegate = self
-        cell.thumbnail.tag = indexPath.row
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath.row)
+        //add thumbnail and caption cell
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ThumbnailAndCaptionCell", for: indexPath) as! ThumbnailAndCaptionCell
+            cell.thumbnailAndCaptionCellDelegate = self
+            cell.thumbnail.tag = indexPath.row
+            cell.thumbnail.contentMode = .scaleAspectFit
+            return cell
+        }
+        return UITableViewCell()
+        
     }
 }
 
@@ -53,8 +69,6 @@ extension NewPostViewController: PHPickerViewControllerDelegate {
                 if let image = object as? UIImage {
                     DispatchQueue.main.async {
                         // Use UIImage
-                        print("Selected image: \(image)")
-                        print("I'm here")
                         cell.thumbnail.setImage(image.withRenderingMode(.alwaysOriginal), for: .normal)
                     }
                 }
---
+++ Pods/Target Support Files/Pods-Finstagram/Pods-Finstagram-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Finstagram : NSObject
+@end
+@implementation PodsDummy_Pods_Finstagram
+@end
