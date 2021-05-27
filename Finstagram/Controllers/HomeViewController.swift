2273b987b7cae80f34cfc772afd284d225b384f5
Darian Low <dlow3@jhu.edu>
2021-05-26 18:29:54 -0700

    added photo support and saving to a custom library

--- Finstagram/Controllers/HomeViewController.swift
+++ Finstagram/Controllers/HomeViewController.swift
@@ -13,6 +13,30 @@ class HomeViewController: UIViewController{
     let postBrain = PostBrain()
     var posts = [Post]()
     
+    //variables for floating button
+    let activityIndicator = UIActivityIndicatorView(style: .gray)
+
+    private var floatingButton: UIButton?
+    private let floatingButtonImageName = "NAME OF YOUR IMAGE"
+    private static let buttonHeight: CGFloat = 75.0
+    private static let buttonWidth: CGFloat = 75.0
+    private let roundValue = HomeViewController.buttonHeight/2
+    private let trailingValue: CGFloat = 15.0
+    private let leadingValue: CGFloat = 15.0
+    private let shadowRadius: CGFloat = 2.0
+    private let shadowOpacity: Float = 0.5
+    private let shadowOffset = CGSize(width: 0.0, height: 5.0)
+    private let scaleKeyPath = "scale"
+    private let animationKeyPath = "transform.scale"
+    private let animationDuration: CFTimeInterval = 0.4
+    private let animateFromValue: CGFloat = 1.00
+    private let animateToValue: CGFloat = 1.05
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        createFloatingButton()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,6 +46,17 @@ class HomeViewController: UIViewController{
         
         postTableView.register(UINib(nibName: "PostCell", bundle: nil), forCellReuseIdentifier: "PostReusableCell")
     }
+    
+    public override func viewWillDisappear(_ animated: Bool) {
+        guard floatingButton?.superview != nil else {return}
+        DispatchQueue.main.async {
+            self.floatingButton?.removeFromSuperview()
+            self.floatingButton = nil
+        }
+        super.viewWillDisappear(animated)
+    }
+    
+    
 }
 
 //MARK: - UITableViewDelegate and UITableViewDataSource
@@ -44,6 +79,120 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath.row)
+    }
+}
+
+//MARK: - UINavigationControllerDelegate and UIImgePickerControllerDelegate
+
+extension HomeViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+
+        // print out the image size as a test
+        print(image.size)
+        saveToAlbum(named: "test", image: image)
+        //UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError err: Error?, contextInfo: UnsafeRawPointer) {
+        activityIndicator.stopAnimating()
+        if let err = err {
+            // we got back an error!
+            presentAlert(title: "Error", message: err.localizedDescription)
+        } else {
+            presentAlert(title: "Saved!", message: "Image saved successfully")
+        }
+    }
+    
+    func presentAlert(title: String, message: String) {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alert, animated: true)
+        }
+        
+    }
+    
+    func saveToAlbum(named: String, image: UIImage) {
+        let album = CustomAlbum(name: named)
+        album.save(image: image) { (result) in
+            switch result {
+            case .success(_):
+                self.presentAlert(title: "Success", message: "Successfully save photo to album \"\(named)\"")
+            case .failure(let err):
+                self.presentAlert(title: "Error", message: err.localizedDescription)
+            }
+        }
+    }
+    
+    private func createFloatingButton() {
+        floatingButton = UIButton(type: .custom)
+        floatingButton?.translatesAutoresizingMaskIntoConstraints = false
+        floatingButton?.backgroundColor = .white
+        floatingButton?.setImage(UIImage(named: floatingButtonImageName), for: .normal)
+        floatingButton?.addTarget(self, action: #selector(openCameraButtonSelected(_:)), for: .touchUpInside)
+        constrainFloatingButtonToWindow()
+        makeFloatingButtonRound()
+        addShadowToFloatingButton()
+        addScaleAnimationToFloatingButton()
+    }
+    
+    
+    
+    private func constrainFloatingButtonToWindow() {
+        DispatchQueue.main.async {
+            guard let keyWindow = UIApplication.shared.keyWindow,
+                let floatingButton = self.floatingButton else { return }
+            keyWindow.addSubview(floatingButton)
+            keyWindow.trailingAnchor.constraint(equalTo: floatingButton.trailingAnchor,
+                                                constant: self.trailingValue).isActive = true
+            keyWindow.bottomAnchor.constraint(equalTo: floatingButton.bottomAnchor,
+                                              constant: self.leadingValue).isActive = true
+            floatingButton.widthAnchor.constraint(equalToConstant:
+                HomeViewController.buttonWidth).isActive = true
+            floatingButton.heightAnchor.constraint(equalToConstant:
+                HomeViewController.buttonHeight).isActive = true
+        }
+    }
+
+    private func makeFloatingButtonRound() {
+        floatingButton?.layer.cornerRadius = roundValue
+    }
+
+    private func addShadowToFloatingButton() {
+        floatingButton?.layer.shadowColor = UIColor.black.cgColor
+        floatingButton?.layer.shadowOffset = shadowOffset
+        floatingButton?.layer.masksToBounds = false
+        floatingButton?.layer.shadowRadius = shadowRadius
+        floatingButton?.layer.shadowOpacity = shadowOpacity
+    }
+
+    private func addScaleAnimationToFloatingButton() {
+        // Add a pulsing animation to draw attention to button:
+        DispatchQueue.main.async {
+            let scaleAnimation: CABasicAnimation = CABasicAnimation(keyPath: self.animationKeyPath)
+            scaleAnimation.duration = self.animationDuration
+            scaleAnimation.repeatCount = .greatestFiniteMagnitude
+            scaleAnimation.autoreverses = true
+            scaleAnimation.fromValue = self.animateFromValue
+            scaleAnimation.toValue = self.animateToValue
+            self.floatingButton?.layer.add(scaleAnimation, forKey: self.scaleKeyPath)
+        }
+    }
+    // TODO: Add some logic for when the button is tapped.
+    @IBAction private func openCameraButtonSelected(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.allowsEditing = true
+        vc.delegate = self
+        present(vc, animated: true)
     }
 }
 
